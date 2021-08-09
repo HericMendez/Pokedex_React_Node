8a1a34dd0ec544ab87328638a73790fd6a3c1148
HericMendez <heric.mendez00@gmail.com>
2021-08-09 20:28:17 -0300

    Front-end em React iniciado, SPA exibindo os pokémons em cards

--- Back-end/public/script.js
+++ Back-end/public/script.js
@@ -1,36 +1,96 @@
-window.onload = pesquisa()
-    async function pesquisa(){
-    try{
-      // const token =  localStorage.getItem('token')
-       const result = await fetch('http://localhost:3000/pokedex/name',{
-           headers:{
-               "Content-Type": "application/json",
-               //"authorization": token
-           },
-       });
-       const json = await result.json()
-       for (var i = 0; i < json.length; i++){
-            var div = document.createElement("div");
-            /*
-            var name = document.createTextNode(json[i].name + "|    |");
-            var type = document.createTextNode(json[i].type + "|    |");
-            var description = document.createTextNode(json[i].description + "|    |");
 
-            div.appendChild(name);
-            div.appendChild(type);
-            div.appendChild(description);
-
-            var divAtual = document.getElementById("div1");
-            document.body.insertBefore(div, divAtual);
-            */
-
-            var name = document.getElementById('name').value;
-            console.log(json[0])
-       }
-    }catch(err){
-        console.log(err)
-    }
+async function getContent(){
+  let name = document.getElementById("fname").value;
+  try{
+      const response = await fetch(`http://localhost:5000/pokedex/${name}`)
+     // console.log(response)
+      const data = await response.json()
+      console.log(data)
+      show(data)
+      findPic()      
+      
+  }catch(error){
+      console.error(error);
+      location.reload()
   }
-  function recarregar(){
-    window.location.replace("http://127.0.0.1:5501/public/index.html");
-  }        
+}
+
+
+const findPic = () => {
+  let imgLink;
+
+  let pokemonName = document.getElementById("fname").value;
+
+  imgLink = `https://img.pokemondb.net/artwork/${pokemonName.toLowerCase()}.jpg`;
+ 
+  const img = document.getElementById("imageid");
+  img.setAttribute("src", imgLink);
+  img.setAttribute("width", "304");
+  img.setAttribute("height", "228");
+  img.setAttribute("alt", "Faiô");
+  img.setAttribute("style", "border-style: solid; border-radius: 5px; padding: 10px; background-color: white;")
+
+  img.src = imgLink;
+  
+};
+
+
+function show(pokemon){
+    let species = '[insert species here]'
+
+   let profile = ''//`<p>Name: ${pokemon.name}<br>  Type: ${pokemon.type}</p>` 
+  
+   profile += `<ul>`
+   profile += `<h6>Gen1</h6>`
+   profile += `<h4>#${pokemon.number}: ${pokemon.name}</h4>`
+   profile += `<li><strong>Type:</strong> ${pokemon.type}</li>`
+   profile += `<li><strong>Height:</strong> ${pokemon.height}, <strong>Weight:</strong> ${pokemon.weight}</li>`
+   profile += `<li><strong>Species:</strong> ${species}</li>`
+
+
+   profile += `<li><strong>Ability:</strong> ${pokemon.ability}</li>`
+   profile += `<li><strong>Description:</strong> ${pokemon.description}</li>`
+   profile += `</ul>`
+
+   let atk = `
+   
+   <ul>
+   <h6>Offensive Attributes:</h6>
+   <li><strong>Super Effective:</strong></li>
+   <p>${pokemon.superEffective}</p>
+   <li><strong>Not Very Effective:</strong></li>
+   <p>${pokemon.notVeryEffective}</p>
+   <li><strong>Has no Effect:</strong></li>
+   <p>${pokemon.noEffect}</p>
+
+
+   </ul>
+   
+   `
+
+   let def = `
+   
+   <ul>
+   <h6>Defensive Attributes:</h6>
+   <li><strong>Resist:</strong></li>
+   <p>${pokemon.resist}</p>
+   <li><strong>Weak:</strong></li>
+   <p>${pokemon.weak}</p>
+   <li><strong>Immune:</strong></li>
+   <p>${pokemon.immune}</p>
+
+
+   </ul>
+   
+   `
+   
+
+
+
+   
+ 
+   
+ document.getElementById('output').innerHTML = profile
+ document.getElementById('atk').innerHTML = atk
+ document.getElementById('def').innerHTML = def
+}
